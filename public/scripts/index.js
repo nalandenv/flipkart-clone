2e9ac0cb5a6bed1836e7c067b131f6b6003e4274
Nikhil Nalande <55831609+nalandenv@users.noreply.github.com>
2021-09-02 17:56:08 +0530

    add function of switching between login and sign up form

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -8,5 +8,47 @@ login.addEventListener("click", () => {
 popUpForm.addEventListener("click", (e) => {
   if (!formContainer.contains(e.target)) {
     popUpForm.classList.add("hidden");
+    resetForm();
   }
 });
+
+// form alter
+const signUp = document.querySelector("#sign-up-link");
+const heading = document.querySelector(".left-sidebar h1");
+const description = document.querySelector(".left-sidebar p");
+const or = document.querySelector("#or");
+const formElement = document.querySelectorAll(
+  ".right-sidebar div .form-element"
+);
+const loginBtn = document.querySelector("#login-btn");
+const otpBtn = document.querySelector("#otp-btn");
+const signInBtn = document.querySelector("#sign-in-btn");
+signUp.addEventListener("click", (e) => {
+  e.preventDefault();
+  or.classList.add("hidden");
+  heading.innerText = "Looks like you're new here!";
+  description.innerText = "Sign up with your mobile number to get started";
+  formElement[1].classList.add("hidden");
+  formElement[0].childNodes[3].innerText = "Enter Mobile Number";
+  loginBtn.innerText = "CONTINUE";
+  otpBtn.classList.add("hidden");
+  signUp.classList.add("hidden");
+  signInBtn.classList.remove("hidden");
+});
+
+signInBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  resetForm();
+});
+const resetForm = () => {
+  or.classList.remove("hidden");
+  heading.innerText = "Login";
+  description.innerText =
+    "Get access to your Orders, Wishlist and Recommendations";
+  formElement[1].classList.remove("hidden");
+  formElement[0].childNodes[3].innerText = "Enter Email/Phone Number";
+  loginBtn.innerText = "Login";
+  otpBtn.classList.remove("hidden");
+  signUp.classList.remove("hidden");
+  signInBtn.classList.add("hidden");
+};
